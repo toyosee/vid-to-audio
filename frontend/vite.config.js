ebcda0953212c963ae912fa57b0a8a4581493674
Elijah Abolaji <tyabolaji@gmail.com>
2026-07-10 10:20:01 +0100

    Update for mp3 frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://vid-to-audio-production.up.railway.app/',
         changeOrigin: true,
       },
     },
