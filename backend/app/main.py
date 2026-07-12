fac79d715e0d0b043e0ac6dec136d17434201e02
Elijah Abolaji <tyabolaji@gmail.com>
2026-07-12 21:30:57 +0100

    CORS issue updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://mp3-grab.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
